6ad9421daedf28009a2410e1c587d63fdae20a89
Yash Vijay Bombale <yashbombale@gmail.com>
2024-02-20 20:54:00 +0530

    complete getallusers API route

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,1 +1,15 @@
+import errorHandler from "../middleware/errorHandler.js";
+import User from "../models/user.model.js";
 
+export const getAllUsers = async (req, res, next) => {
+  try {
+    const { id: loggedInUserId } = req.user;
+    const users = await User.find({ _id: { $ne: loggedInUserId } }).select(
+      "-password"
+    ); //$ne is used because it will return all users except loggedInUser because we dont want to allow users to chat with user himself
+    if (!users) return next(errorHandler(404, "No users found!"));
+    return res.status(200).json(users);
+  } catch (error) {
+    next(error);
+  }
+};
--- server/routers/user.route.js
+++ server/routers/user.route.js
@@ -1,4 +1,8 @@
 import express from "express";
+import isAuthenticated from "../middleware/auth.middleware.js";
+import { getAllUsers } from "../controllers/user.controller.js";
 const router = express.Router();
+
+router.get("/getallusers", isAuthenticated, getAllUsers);
 
 export default router;
